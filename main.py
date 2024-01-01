cc8115ff8f0b592896c36670d4f9cf871785f25b
rotany <rotany38@gmail.com>
2024-01-01 12:09:55 +0100

    cors

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PyPDF2 import PdfReader
 from flask import Flask , request, jsonify
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app, resources={r"/cv-analyzer": {"origins": "https://nuncaestarde.blog"}})
 
 
 def verificar_apto(archivo_pdf, archivo_palabras_clave):
